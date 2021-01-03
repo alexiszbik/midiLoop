835d38136ad8ca7fba361e49d2b832cabaa04d1d
Alexis Zbik <alexis.zbik@gmail.com>
2021-01-03 18:09:35 +0100

    handle bar count knob handle step count knob handle play & stop midi out send PLAY, STOP & SYNC tempo max 240 was really to much send note off when stopped

--- midiLoop.ino
+++ midiLoop.ino
@@ -2,6 +2,8 @@
 #include <MIDI.h>
 
 #define TEMPO_ANALOG_IN 0
+#define BARCOUNT_ANALOG_IN 1
+#define STEPCOUNT_ANALOG_IN 2
 #define MIDITHRU_ANALOG_IN 3
 #define TRANSPOSE_ANALOG_IN 4
 #define SHIFT_ANALOG_IN 5
@@ -21,19 +23,33 @@
 #define CHANNEL_2_PIN 10
 #define CHANNEL_1_PIN 11
 
-#define BEATOUT_PIN 12
+#define BEATOUT_LED 12
 
 #define CHANNEL_COUNT 4
-#define SEQUENCE_LENGTH 32
+#define SEQUENCE_LENGTH_MAX 128
+#define STEP_PER_BAR_MAX 16
 
 #define TEMPO_MIN 30
-#define TEMPO_MAX 240
+#define TEMPO_MAX 200
+
+byte ledPins[4] = {CHANNEL_1_LED, CHANNEL_2_LED, CHANNEL_3_LED, CHANNEL_4_LED};
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 bool shiftIsPressed = false;
 
-bool currentPlayStopSwitchState = false;
+unsigned long delayStart = 0;
+bool delayIsRunning = false;
+
+int currentBarCount = 1;
+int currentStepCount = STEP_PER_BAR_MAX;
+
+int currentSeqLength = 16;
+
+bool isPlaying = false;
+bool isPlayingSwitchState = false;
+
+bool needsToSendMidiStart = false;
 
 bool midiThru = false;
 bool transposeMode = false;
@@ -43,14 +59,18 @@ byte transpose[CHANNEL_COUNT];
 byte baseNote = 60;
 
 byte currentPosition = 0;
-byte sequence[CHANNEL_COUNT][SEQUENCE_LENGTH];
+byte sequence[CHANNEL_COUNT][SEQUENCE_LENGTH_MAX];
 
 byte previousNote[CHANNEL_COUNT];
 
 void clockOutput16PPQN(uint32_t* tick) {
 
-  bool isQuarterBeat = ((currentPosition % 4) == 0);
-  digitalWrite(BEATOUT_PIN, isQuarterBeat); 
+  if (!isPlaying) {
+    return;
+  }
+
+  bool isQuarterBeat = (((currentPosition % currentStepCount) % 4) == 0);
+  digitalWrite(BEATOUT_LED, isQuarterBeat); 
 
   for (size_t channel = 0; channel < CHANNEL_COUNT; channel++) {
       if (previousNote[channel] > 0) {
@@ -68,12 +88,25 @@ void clockOutput16PPQN(uint32_t* tick) {
       }
   }
 
-  currentPosition = (currentPosition + 1) % SEQUENCE_LENGTH;
+  currentPosition = (currentPosition + 1) % currentSeqLength;
 }
 
 void clockOutput32PPQN(uint32_t* tick) {
-  digitalWrite(OUT_GATE, (*tick % 2) == 0);
-  digitalWrite(OUT_SYNC, (*tick % 4) < 2);
+
+  if (!isPlaying) {
+    return;
+  }
+  
+  digitalWrite(OUT_GATE, (*tick % 2) == 0 ? HIGH : LOW);
+  digitalWrite(OUT_SYNC, (*tick % 4) < 2 ? HIGH : LOW);
+}
+
+void clockOutput96PPQN(uint32_t* tick) {
+  if (needsToSendMidiStart) {
+    needsToSendMidiStart = false;
+    Serial.write(0xFA);
+  }
+  Serial.write(0xF8);
 }
 
 void setup() {
@@ -83,11 +116,12 @@ void setup() {
 
   uClock.setClock16PPQNOutput(clockOutput16PPQN);
   uClock.setClock32PPQNOutput(clockOutput32PPQN);
+  uClock.setClock96PPQNOutput(clockOutput96PPQN);
 
   uClock.setTempo(96);
   uClock.start();
 
-  for (size_t i = 0; i < SEQUENCE_LENGTH; i++) {
+  for (size_t i = 0; i < SEQUENCE_LENGTH_MAX; i++) {
     previousNote[i] = 0;
     for (size_t channel = 0; channel < CHANNEL_COUNT; channel++) {
       sequence[channel][i] = 0;
@@ -108,7 +142,10 @@ void setup() {
   pinMode(CHANNEL_3_PIN, INPUT);
   pinMode(CHANNEL_4_PIN, INPUT);
 
-  pinMode(BEATOUT_PIN, OUTPUT);
+  pinMode(BEATOUT_LED, OUTPUT);
+
+  pinMode(OUT_GATE, OUTPUT);
+  pinMode(OUT_SYNC, OUTPUT);
 
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
@@ -129,7 +166,7 @@ void handleErase() {
       if (!shiftIsPressed) {
         sequence[currentChannel][currentPosition] = 0;
       } else {
-        for (size_t step = 0; step < SEQUENCE_LENGTH; step++) {
+        for (size_t step = 0; step < SEQUENCE_LENGTH_MAX; step++) {
           sequence[currentChannel][step] = 0;
         }
       }
@@ -137,11 +174,49 @@ void handleErase() {
 }
 
 void handleTempo() {
-  int tempoPot = analogRead(0);
+  int tempoPot = analogRead(TEMPO_ANALOG_IN);
   float tempo = round(((float)tempoPot/1024.f)*(TEMPO_MAX - TEMPO_MIN) + TEMPO_MIN);
 
   uClock.setTempo(tempo);
 }
+
+void handleBarCount() {
+  int pot = analogRead(BARCOUNT_ANALOG_IN);
+  byte maxValue = SEQUENCE_LENGTH_MAX/STEP_PER_BAR_MAX;
+  int nextBarCount = round(((float)pot/1024.f)*(maxValue - 1) + 1);
+  
+  if (nextBarCount != currentBarCount) {
+    currentBarCount = nextBarCount;
+    currentSeqLength = currentBarCount * currentStepCount;
+
+    displayIntValue(nextBarCount);
+  }
+}
+
+void handleStepCount() {
+  int pot = analogRead(STEPCOUNT_ANALOG_IN);
+  int nextStepCount = round(((float)pot/1024.f)*(STEP_PER_BAR_MAX - 1) + 1);
+
+  if (nextStepCount != currentStepCount) {
+    currentStepCount = nextStepCount;
+    currentSeqLength = currentBarCount * currentStepCount;
+
+    displayIntValue(nextStepCount);
+  }
+}
+
+void displayIntValue(int value) {
+  int b[4] = {1,2,4,8};
+
+   for (int i = 0; i < CHANNEL_COUNT; i++) {
+     int state = value & b[i];
+     digitalWrite(ledPins[i], (state > 0) ? HIGH : LOW);
+   }
+
+   delayStart = millis();
+   delayIsRunning = true;
+}
+
 
 void handleMidiThru() {
   int midiThruState = analogRead(MIDITHRU_ANALOG_IN);
@@ -167,43 +242,76 @@ void handleCurrentChannel() {
     }
   }
 
-  byte ledPins[4] = {CHANNEL_1_LED, CHANNEL_2_LED, CHANNEL_3_LED, CHANNEL_4_LED};
   
-  for (byte i = 0; i < CHANNEL_COUNT; i++) {
-    bool state = i == (currentChannel); 
-    digitalWrite(ledPins[i], state ? HIGH : LOW);
+  if (!delayIsRunning) {
+    for (byte i = 0; i < CHANNEL_COUNT; i++) {
+      bool state = i == (currentChannel); 
+      digitalWrite(ledPins[i], state ? HIGH : LOW);
+    }
   }
-  
-  
-}
 
-void toggleStartStop() {
-  
 }
 
 void handleStartStop() {
-  int switchState = analogRead(PLAYSTOP_ANALOG_IN);
+  int startStopStart = analogRead(PLAYSTOP_ANALOG_IN);
+  bool newPlayingState = (startStopStart > 200);
 
-  if ((switchState == HIGH) && !currentPlayStopSwitchState) {
-    toggleStartStop();
-    currentPlayStopSwitchState = true;
-  }
-  
-  if (switchState == LOW) {
-    currentPlayStopSwitchState = false;
+  if (newPlayingState != isPlayingSwitchState) {
+    isPlayingSwitchState = newPlayingState;
+
+    if (isPlayingSwitchState) {
+
+      if (isPlaying && shiftIsPressed) {
+        currentPosition = 0;
+      } else {
+      
+        isPlaying = !isPlaying;
+
+        if (isPlaying) {
+          uClock.stop();
+          //delay(20);
+          needsToSendMidiStart = true;
+          uClock.start();
+        } else {
+          Serial.write(0xFC);
+
+          for (size_t channel = 0; channel < CHANNEL_COUNT; channel++) {
+            if (previousNote[channel] > 0) {
+              MIDI.sendNoteOn(previousNote[channel], 0, channel + 1);
+              previousNote[channel] = 0;
+            }
+          }
+        
+          digitalWrite(OUT_GATE, LOW);
+          digitalWrite(OUT_SYNC, LOW);
+          digitalWrite(BEATOUT_LED, LOW); 
+          //delay(20);
+          currentPosition = 0;
+        
+        }
+      }
+    }
   }
 }
 
 void loop() {
 
   handleShift();
+  
   handleTempo();
+  handleBarCount();
+  handleStepCount();
+  
   handleMidiThru();
   handleTranspose();
   handleStartStop();
   handleCurrentChannel();
   
   handleErase();
+
+  if ((millis() - delayStart) >= 2000 && delayIsRunning) {
+    delayIsRunning = false;
+  }
 
   //for debug purpose
   //digitalWrite(CHANNEL_1_LED, shiftIsPressed ? HIGH : LOW);
